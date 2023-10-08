3a0cd1b983f24b677e45527db8df9a85c6cfd66a
Hasan Mesbaul Ali Taher <hasanmesbaul420@gmail.com>
2023-10-08 22:37:51 +0600

    Connect mongodb with node.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
 // routes
 app.get('/', (req, res) => {
@@ -10,7 +11,17 @@ app.get('/blog', (req, res) => {
     res.send('Welcome to Node API - Blog. This is a blog page');
 });
 
-// start server to listen to port 3000 and the callback funtion will be called once the server is started
-app.listen(3000, () => {
+
+mongoose
+.connect('mongodb+srv://hasan:hasan@cluster0.wdogl2i.mongodb.net/node_api?retryWrites=true&w=majority&appName=AtlasApp')
+.then(() => {
+    console.log('MongoDB connected');
+    // start server to listen to port 3000 and the callback funtion will be called once the server is started
+    app.listen(3000, () => {
     console.log('server started. Node api is running on: http://localhost:3000');
-});
+    });
+})
+.catch(err =>{
+    console.log(err);
+})
+
