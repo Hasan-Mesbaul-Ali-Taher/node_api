1cb905eb1ee5154a8f75c1ed4fd6d6d28f2a630b
Hasan Mesbaul Ali Taher <hasanmesbaul420@gmail.com>
2023-10-09 13:20:15 +0600

    Fetch all products and product by ID

--- server.js
+++ server.js
@@ -18,7 +18,32 @@ app.get('/blog', (req, res) => {
     res.send('Welcome to Node API - Blog. This is a blog page');
 });
 
-app.post('/product', async(req, res) => {
+app.get('/products', async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+})
+
+// Get product by ID   
+app.get('/products/:id', async(req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        // or
+        // const {id} = req.params;
+        // const product = await Product.findById(id);
+        
+        res.status(200).json(product);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+})
+
+app.post('/products', async(req, res) => {
     // console.log(req.body);
     // res.send(req.body);
     try {
