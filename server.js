fbd9978f6dd9c1d040a95044ba40a6193870dbad
Hasan Mesbaul Ali Taher <hasanmesbaul420@gmail.com>
2023-10-09 13:04:13 +0600

    Use a model (product) to save data in mongoDB

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+// Import product module
+const Product = require('./models/productModel');
+
+// add middleware
+app.use(express.json());
+
+
 // routes
 app.get('/', (req, res) => {
     res.send('Welcome to Node API');
@@ -11,6 +18,18 @@ app.get('/blog', (req, res) => {
     res.send('Welcome to Node API - Blog. This is a blog page');
 });
 
+app.post('/product', async(req, res) => {
+    // console.log(req.body);
+    // res.send(req.body);
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: error.message });
+    }
+})
 
 mongoose
 .connect('mongodb+srv://hasan:hasan@cluster0.wdogl2i.mongodb.net/node_api?retryWrites=true&w=majority&appName=AtlasApp')
