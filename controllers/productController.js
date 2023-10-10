0a757fe42b2c527c769ac2ab4b36e495db154308
Hasan Mesbaul Ali Taher <hasanmesbaul420@gmail.com>
2023-10-10 17:18:13 +0600

    Use express-async-handler for error handling in controller functions

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,18 +1,23 @@
 const Product = require('../models/productModel')
 
+const asyncHandler = require('express-async-handler')
+
+
 // Get all products
-const getProducts = async(req, res) => {
+const getProducts = asyncHandler(async(req, res) => {
     try {
         const products = await Product.find({});
         res.status(200).json(products);
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
+        // console.log(error.message);
+        // res.status(500).json({ message: error.message });
     }
-}
+})
 
 // Get a product by ID
-const getProductById = async(req, res) => {
+const getProductById = asyncHandler(async(req, res) => {
     try {
         const product = await Product.findById(req.params.id);
         // or
@@ -21,43 +26,53 @@ const getProductById = async(req, res) => {
 
         res.status(200).json(product);
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
+        // console.log(error.message);
+        // res.status(500).json({ message: error.message });
     }
-}
+})
 
 // Update a product by ID
-const updateProductById = async(req, res) => {   
+const updateProductById = asyncHandler(async(req, res) => {   
     try {
         const {id} = req.params;
         const product = await Product.findByIdAndUpdate(id, req.body);
         // WE can not find any product with this id in database
         if(!product) {
-            return res.status(404).json({ message: `Product not found with ID ${id}` });
+            res.status(404);
+            throw new Error(`Product not found with ID ${id}`);
+            // return res.status(404).json({ message: `Product not found with ID ${id}` });
         }
         const updatedProduct = await Product.findById(id);
         res.status(200).json(updatedProduct);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
+        // res.status(500).json({ message: error.message });
     }
-}
+})
 
 // Delete a product by ID
-const deleteProductById = async(req, res) => {
+const deleteProductById = asyncHandler(async(req, res) => {
     try {
         const {id} = req.params;
         const product = await Product.findByIdAndDelete(id);
         if(!product) {
-            return res.status(404).json({ message: `Product not found with ID ${id}` });
+            res.status(404);
+            throw new Error(`Product not found with ID ${id}`);
+            // return res.status(404).json({ message: `Product not found with ID ${id}` });
         }
         res.status(200).json(product);
     } catch (error) {
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
+        // res.status(500).json({ message: error.message });
     }
-}
+})
 
 // Create a new product
-const createProduct = async(req, res) => {
+const createProduct = asyncHandler(async(req, res) => {
     // console.log(req.body);
     // res.send(req.body);
     try {
@@ -65,10 +80,14 @@ const createProduct = async(req, res) => {
         res.status(200).json(product);
 
     } catch (error) {
-        console.log(error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500);
+        throw new Error(error.message);
+        // console.log(error.message);
+        // res.status(500).json({ message: error.message });
     }
-}
+})
+
+
 
 module.exports = {
     getProducts,
@@ -76,5 +95,5 @@ module.exports = {
     updateProductById,
     deleteProductById,
     createProduct,
-    
+
 }
