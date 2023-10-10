ed7af0b9f188defd7cce954e823c9e7a749f954f
Hasan Mesbaul Ali Taher <hasanmesbaul420@gmail.com>
2023-10-10 17:45:12 +0600

    Set up CORS policy

--- server.js
+++ server.js
@@ -4,9 +4,25 @@ const app = express();
 const mongoose = require('mongoose');
 const productRoute = require('./routes/productRoute');
 const errorMiddleware = require('./middleware/errorMiddleware');
+const cors = require('cors');
 // 
 const MONGO_URL = process.env.MONGO_URL
 const PORT = process.env.PORT || 3000;
+const FRONTEND = process.env.FRONTEND;
+// use cors middleware
+// app.use(cors({ origin: '*' })); // this allows all origins. that is anyone can access your backend api
+// app.use(cors()); // this allows all origins. that is anyone can access your backend api
+// app.use(cors({ origin: 'http://localhost:3000' })); // this allows only http://localhost:3000 to access your backend api
+
+const corsOptions = {
+    // origin: 'http://example.com',
+    // origin: ['http://localhost:3000', 'http://example.com'], // this allows multiple origins to access your backend api
+    // origin: 'http://localhost:3000', // this allows only http://localhost:3000 to access your backend api
+    
+    origin: FRONTEND, 
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // add middleware
 app.use(express.json());
